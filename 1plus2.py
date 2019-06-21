d7c671eba41fc1a8af6af136e6b3705cdf1a8a05
jaywang <249735182@qq.com>
2019-06-21 09:56:48 +0800

    输入错误后重新输入

--- 1plus2.py
+++ 1plus2.py
@@ -1,18 +1,39 @@
-#  高级版作业
-try:
-    num1 = int(input("请输入第一个数字: "))
-except Exception as e:
-    print("请重新输入一个整数：")
-    raise e
+# #  高级版作业
+# try:
+#     num1 = int(input("请输入第一个数字: "))
+# except Exception as e:
+#     print("请重新输入一个整数：")
+#     raise e
+#
+# try:
+#     num2 = int(input("请输入第二个数字: "))
+# except Exception as e:
+#     print("请重新输入一个整数：")
+#     raise e
+#
+# result = num1 + num2
+#
+# print("您输入的数字有误，请重新运行")
+# print("计算%d+%d的结果" %(num1, num2))
+# print("计算结果是: %d" %(result))
 
-try:
-    num2 = int(input("请输入第二个数字: "))
-except Exception as e:
-    print("请重新输入一个整数：")
-    raise e
 
+while True:
+    try:
+        num1 = int(input("第一个数字："))
+
+        break
+    except:
+        print("输入错误,请重新输入一个整数\r\n")
+
+while True:
+    try:
+        num2 = int(input("第二个数字:"))
+        break
+    except:
+        print("输入错误,请重新输入一个整数")
 result = num1 + num2
-
-print("您输入的数字有误，请重新运行")
 print("计算%d+%d的结果" %(num1, num2))
 print("计算结果是: %d" %(result))
+
+
